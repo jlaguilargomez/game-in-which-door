710e32d6830963e3a263d66a191f6153d309e103
Jose Luis Aguilar <49944214+jlaguilargomez@users.noreply.github.com>
2019-09-12 17:43:35 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,107 @@
+let doorImage1 = document.getElementById('door1');
+let doorImage2 = document.getElementById('door2');
+let doorImage3 = document.getElementById('door3');
+let startButton = document.getElementById('start');
+let currentlyPlaying = true;
+
+let botDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg"
+let beachDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg"
+let spaceDoorPath ="https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg"
+let closedDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/closed_door.svg"
+
+const playDoor = (door) =>{
+  numClosedDoors--;
+  if(numClosedDoors == 0){
+    console.log(numClosedDoors); 
+    gameOver('win');   
+  }else if(isBot(door)){
+  	gameOver();        
+  }
+}
+
+const isClicked = (door) =>{
+  if(door.src === closedDoorPath){
+     return false;
+  }else{
+     return true;
+  };
+}
+
+const isBot = (door) =>{
+  if(door.src==botDoorPath){
+     return true;
+  }else{
+    return false;
+  }
+}
+
+//random place for robot
+let numClosedDoors=3;
+let openDoor1;
+let openDoor2;
+let openDoor3;
+
+const randomChoreDoorGenerator=()=>{
+  let choreDoor = Math.floor(Math.random()*numClosedDoors);
+  if(choreDoor==0){
+    openDoor1 = botDoorPath;
+    openDoor2 = beachDoorPath;
+    openDoor3 = spaceDoorPath;
+  }else if(choreDoor==1){
+    openDoor1 = beachDoorPath;
+    openDoor2 = spaceDoorPath;
+    openDoor3 = botDoorPath;
+  }else{
+    openDoor1 = spaceDoorPath;
+    openDoor2 = botDoorPath;
+    openDoor3 = beachDoorPath;
+
+  }
+};
+
+doorImage1.onclick =()=>{
+  if(currentlyPlaying && !isClicked(doorImage1)){
+    doorImage1.src = openDoor1; 
+  	playDoor(doorImage1);  
+  }
+};
+
+doorImage2.onclick =()=>{
+  if(currentlyPlaying && !isClicked(doorImage2)){
+    doorImage2.src = openDoor2; 
+  	playDoor(doorImage2);  
+  }
+};
+doorImage3.onclick =()=>{
+  if(currentlyPlaying && !isClicked(doorImage3)){
+    doorImage3.src = openDoor3; 
+  	playDoor(doorImage3);  
+  }
+};
+
+startButton.onclick = () =>{
+  startRound();
+}
+
+
+function startRound(){
+  doorImage1.src = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/closed_door.svg";
+  doorImage2.src = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/closed_door.svg";
+  doorImage3.src = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/closed_door.svg";
+  numClosedDoors = 3;
+  startButton.innerHTML = "Good luck!"
+  currentlyPlaying = true;
+  randomChoreDoorGenerator();
+}
+
+
+const gameOver = (status) =>{
+  if(status == 'win'){
+  	startButton.innerHTML = 'You win, Play again?'
+  }else{
+    startButton.innerHTML = 'Game Over! Play again?'
+  }
+  currentlyPlaying = false;
+}
+
+randomChoreDoorGenerator();
